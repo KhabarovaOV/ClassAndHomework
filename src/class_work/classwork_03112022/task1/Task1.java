aa92a5b94b50e9d613b33818906e3b0634b2706d
Olya <olga.khabarova2013@yandex.ru>
2022-11-09 11:19:22 +0300

    finish classwork task1

--- src/class_work/classwork_03112022/task1/Task1.java
+++ src/class_work/classwork_03112022/task1/Task1.java
@@ -9,8 +9,20 @@ public class Task1 {
 
     List<Integer> arrayList = new ArrayList<>();
     HashMap<Integer, String> numberOfVarious = new HashMap<>();
+    int[] num;
+    private boolean endFlag = true;
+
+    public static void main(String[] args) {
+        Task1 task1 = new Task1();
+        task1.getText();
+
+        do {
+            task1.variousOfDo();
+        } while (task1.endFlag);
+    }
 
     public void createdMap() {
+        System.out.println("Выберете пункт из меню: ");
         numberOfVarious.put(1, "Добавить элемент в список");
         numberOfVarious.put(2, "Удалить элемент из списка");
         numberOfVarious.put(3, "Показать содержимое списка");
@@ -18,7 +30,6 @@ public class Task1 {
         numberOfVarious.put(5, "Заменить значение в списке");
         numberOfVarious.put(6, "Выход");
         System.out.println(numberOfVarious);
-        variousOfDo();
     }
 
     public int getMenuPoint() {
@@ -26,32 +37,27 @@ public class Task1 {
     }
 
     private void variousOfDo() {
+        createdMap();
         int menuPoint = getMenuPoint();
         switch (menuPoint) {
             case 1:
                 addNewNumber();
-                createdMap();
-                ;
+                break;
             case 2:
                 deleteElement();
-                createdMap();
-                ;
+                break;
             case 3:
                 watchList();
-                createdMap();
-                ;
+                break;
             case 4:
                 chekList();
-                createdMap();
-                ;
+                break;
             case 5:
                 changedElement();
-                createdMap();
-                ;
+                break;
             case 6:
                 System.out.println("Программа завершена!");
-                break;
-            default:
+                endFlag = false;
         }
     }
 
@@ -60,19 +66,13 @@ public class Task1 {
         return scanner.nextInt();
     }
 
-    public void createdList() {
-        arrayList.add(scan());
-    }
-
-
     public void addNewNumber() {
-        scan();
+        System.out.println("Введите число, которое хотите добавить: ");
         arrayList.add(scan());
     }
 
     public void deleteElement() {
-        System.out.println("Выберете номер элемента, который хотите удалить: ");
-        scan();
+        System.out.println("Выберете номер элемента, который хотите удалить(отсчет идет от нуля): ");
         arrayList.remove(scan());
     }
 
@@ -83,27 +83,38 @@ public class Task1 {
     }
 
     public void chekList() {
-        scan();
+        System.out.println("Введите число, которое хотите проверить на наличие в списке: ");
         boolean contains = arrayList.contains(scan());
         System.out.println(contains);
     }
 
-    public void changedElement(){
-        System.out.println("Введите порядковый номер элемента, который хотите удалить: ");
+    public void changedElement() {
+        System.out.println("Введите порядковый номер элемента, который хотите удалить(отсчет идет от нуля): ");
         Scanner scannerOne = new Scanner(System.in);
         Integer k = scannerOne.nextInt();
-        System.out.println("Введите значение нового номера: ");
+        System.out.println("Введите номер, который хотите добавить: ");
         Scanner scannerTwo = new Scanner(System.in);
         Integer i = scannerTwo.nextInt();
         arrayList.set(k, i);
     }
-    public static void main(String[] args) {
-        Task1 task1 = new Task1();
-        System.out.println("Введите числа: ");
-        task1.createdList();
-        System.out.println("Выберете пункт из меню: ");
-        task1.createdMap();
 
+    public void getText() {
+        Scanner sc = new Scanner(System.in);
+        String text = null;
+        try {
+            System.out.println("Введите числа через пробел: ");
+            text = sc.nextLine();
+
+            String[] text1 = text.split(" ");
+            num = new int[text1.length];
+            for (int i = 0; i < num.length; i++) {
+                num[i] = Integer.parseInt(text1[i]);
+                arrayList.add(num[i]);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println(text + " это не число");
+            getText();
+        }
     }
 
 }
